9fcd84b0b1688d9050d8d3c568ea378b95f9c802
Anu Mathews <111149534+anumathewse@users.noreply.github.com>
2022-08-29 00:18:26 -0400

    Update Test01_GET.java
    Added Assert True for service response time

--- RestAssuredProject/src/test/java/com/sample/restassured/Test01_GET.java
+++ RestAssuredProject/src/test/java/com/sample/restassured/Test01_GET.java
@@ -1,7 +1,10 @@
 package com.sample.restassured;
 
+import java.util.concurrent.TimeUnit;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
@@ -16,6 +19,16 @@ public class Test01_GET
 		
 		System.out.println(response.getStatusCode());
 		System.out.println(response.getBody().asString());		
+		
+		long responseTime = response.getTime();
+		
+		System.out.println(responseTime);	
+		
+		// Validating response time is less than or equals (in Milliseconds) 
+		Assert.assertTrue(responseTime <= 1000);
+					
+		// Validating response time is less than or equals (in Seconds) with custom message
+		Assert.assertTrue(TimeUnit.MILLISECONDS.toSeconds(responseTime) <= 1, "Valid service response time : less than 1 second");
 	}
 	
 	@Test
