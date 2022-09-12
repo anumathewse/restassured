46b33dd69b5c4a8f1718d650cfd3a92820338c51
Anu Mathews <111149534+anumathewse@users.noreply.github.com>
2022-09-11 23:58:54 -0400

    Header Key-Values
    Validate Header Key-Values

--- RestAssuredProject/src/test/java/com/sample/restassured/Test01_GET.java
+++ RestAssuredProject/src/test/java/com/sample/restassured/Test01_GET.java
@@ -6,6 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
 import io.restassured.response.Response;
 
 public class Test01_GET 
@@ -18,17 +19,25 @@ public class Test01_GET
 		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
 		
 		System.out.println(response.getStatusCode());
-		System.out.println(response.getBody().asString());		
+		System.out.println(response.getBody().asString());	
+		
+		
+		for (Header header : response.getHeaders()) {
+		
+			System.out.println("==> " +header.getName() + " : " + header.getValue());
+			
+		}
+		
 		
 		long responseTime = response.getTime();
 		
 		System.out.println(responseTime);	
 		
 		// Validating response time is less than or equals (in Milliseconds) 
-		Assert.assertTrue(responseTime <= 1000);
+		//Assert.assertTrue(responseTime <= 1000);
 					
 		// Validating response time is less than or equals (in Seconds) with custom message
-		Assert.assertTrue(TimeUnit.MILLISECONDS.toSeconds(responseTime) <= 1, "Valid service response time : less than 1 second");
+		//Assert.assertTrue(TimeUnit.MILLISECONDS.toSeconds(responseTime) <= 1, "Valid service response time : less than 1 second");
 	}
 	
 	@Test
